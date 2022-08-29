3660d1852803e5bb88ae52eb775b0530d6d12185
TheresaQWQ <yxiao2044@gmail.com>
2022-08-29 14:18:43 +0800

    图片缩放，移动和删除选区功能

---
+++ editor.js
@@ -0,0 +1,302 @@
+class Editor {
+  constructor (e) {
+    this.element = typeof e === 'string' ? document.querySelector(e) : e
+    this.element.style.position = 'relative'
+
+    this.screen_width = this.element.offsetWidth
+    this.screen_height = this.element.offsetHeight
+    this.background = this._createCanvas(0)
+    this.display = this._createCanvas(1)
+
+    this.mouse_state = {}
+    this.keyboard_state = {}
+
+    document.addEventListener('keydown', event => {
+      const key = event.key.toLowerCase()
+      this.keyboard_state[key] = true
+
+      if (key === 'delete') this.delete()
+
+      console.log(`[keydown] ${key}`)
+    })
+
+    document.addEventListener('keyup', event => {
+      const key = event.key.toLowerCase()
+      delete this.keyboard_state[key]
+      console.log(`[keyup] ${key}`)
+    })
+
+    this.element.onmousedown = event => {
+      const key = event.button
+      this.mouse_state[key] = [event.offsetX, event.offsetY]
+      console.log(`[mousedown] ${key}, x: ${event.offsetX}, y: ${event.offsetY}`)
+    }
+
+    this.element.onmouseup = event => {
+      const key = event.button
+      delete this.mouse_state[key]
+      console.log(`[mouseup] ${key}`)
+    }
+
+    this._initDisplay()
+
+    this.element.onmousewheel = event => {
+      event.preventDefault()
+      event.stopPropagation()
+      const offset = event.wheelDelta / 120 / 50
+      this.state_scale += offset
+      this.scale(this.state_scale)
+    }
+
+    this.element.onmousemove = event => {
+      if (this.mouse_state[0] && this.keyboard_state[' ']) {
+        event.preventDefault()
+        event.stopPropagation()
+
+        const x = event.offsetX
+        const y = event.offsetY
+        const offsetX = (x - this.mouse_state[0][0]) * -1
+        const offsetY = (y - this.mouse_state[0][1]) * -1
+
+        this.mouse_state[0][0] = x
+        this.mouse_state[0][1] = y
+
+        this.move(offsetX, offsetY)
+      }
+    }
+  }
+
+  _createCanvas (zIndex) {
+    const element = document.createElement('canvas')
+    element.width = this.screen_width
+    element.height = this.screen_height
+    element.style.position = 'absolute'
+    element.style.left = 0
+    element.style.top = 0
+    element.style.zIndex = zIndex
+    this.element.appendChild(element)
+    const ctx = element.getContext('2d')
+    return {
+      element,
+      ctx
+    }
+  }
+
+  _initDisplay () {
+    const start = {
+      x: 0,
+      y: 0,
+      stop: true
+    }
+
+    this.display.element.onmousedown = event => {
+      this.display.ctx.clearRect(0, 0, this.screen_width, this.screen_height)
+      // 清空display画布
+      start.x = event.offsetX
+      start.y = event.offsetY
+      start.stop = false
+    }
+
+    this.display.element.onmousemove = event => {
+      if (start.stop || this.keyboard_state[' ']) return
+      this.display.ctx.clearRect(0, 0, this.screen_width, this.screen_height)
+      const x = event.offsetX
+      const y = event.offsetY
+
+      this.display.ctx.beginPath()
+      this.display.ctx.strokeStyle = '#000'
+      this.display.ctx.moveTo(start.x, start.y)
+      this.display.ctx.lineTo(start.x, y)
+      this.display.ctx.lineTo(x, y)
+      this.display.ctx.lineTo(x, start.y)
+      this.display.ctx.closePath()
+      this.display.ctx.stroke()
+
+      // 绘制半透明的矩形
+      this.display.ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+      this.display.ctx.fillRect(start.x, start.y, x - start.x, y - start.y)
+
+      this.state_selection = {
+        x1: start.x,
+        y1: start.y,
+        x2: x,
+        y2: y
+      }
+    }
+
+    this.display.element.onmouseup = event => {
+      start.stop = true
+      const x1 = start.x
+      const x2 = event.offsetX
+      const y1 = start.y
+      const y2 = event.offsetY
+
+      const diffX = Math.abs(x1 - x2)
+      const diffY = Math.abs(y1 - y2)
+
+      const min = Math.min(diffX, diffY)
+      // 如果选区小于2像素，就清空选区
+      if (min < 2) {
+        console.log('[selection] clear selection')
+        this.state_selection = null
+      }
+    }
+  }
+
+  getCurrentSelection () {
+    if (!this.state_selection) {
+      return {
+        x1: -1,
+        y1: -1,
+        x2: -1,
+        y2: -1
+      }
+    }
+
+    return this.state_selection
+  }
+
+  loadImage (url) {
+    this.image = new Image()
+    this.image.onload = () => {
+      const canvas_width = this.screen_width
+      const canvas_height = this.screen_height
+      const image_width = this.image.width
+      const image_height = this.image.height
+
+      // 如果图片小于画布，就居中
+      if (image_width < canvas_width && image_height < canvas_height) {
+        this.state_scale = 1
+        this.background.ctx.drawImage(this.image, (canvas_width - image_width) / 2, (canvas_height - image_height) / 2)
+      } else {
+        // 缩放后居中
+        const scale = Math.min(canvas_width / image_width, canvas_height / image_height)
+        this.state_scale = scale
+        const width = image_width * scale
+        const height = image_height * scale
+        this.background.ctx.drawImage(this.image, (canvas_width - width) / 2, (canvas_height - height) / 2, width, height)
+      }
+
+      this.state_move_y = 0
+      this.state_move_x = 0
+    }
+
+    this.image.src = url
+  }
+
+  // 转换canvas坐标到图片坐标
+  _transformCanvasToImage (x, y) {
+    const scale = this.state_scale
+    const offsetX = (this.state_move_x || 0) * -1
+    const offsetY = (this.state_move_y || 0) * -1
+
+    // 图片原始大小
+    const image_width = this.image.width
+    const image_height = this.image.height
+
+    // 画布大小
+    const canvas_width = this.screen_width
+    const canvas_height = this.screen_height
+
+    // 缩放后图片大小
+    const width = image_width * scale
+    const height = image_height * scale
+
+    // 缩放后图片左上角坐标
+    const image_x = (canvas_width - width) / 2 - offsetX
+    const image_y = (canvas_height - height) / 2 - offsetY
+
+    // 缩放后相对于图片的坐标
+    const image_dx = x - image_x
+    const image_dy = y - image_y
+
+    // 原始坐标
+    const dx = image_dx / scale
+    const dy = image_dy / scale
+
+    console.log(`[transform] x: ${x} -> ${dx}, y: ${y} -> ${dy}`)
+
+    return {
+      x: dx,
+      y: dy
+    }
+  }
+
+  delete () {
+    const selection = this.getCurrentSelection()
+    if (selection.x1 === -1) return
+
+    const { x: x1, y: y1 } = this._transformCanvasToImage(selection.x1, selection.y1)
+    const { x: x2, y: y2 } = this._transformCanvasToImage(selection.x2, selection.y2)
+
+    console.log(`[delete] x1: ${x1}, y1: ${y1}, x2: ${x2}, y2: ${y2}`)
+
+    const canvas = document.createElement('canvas')
+    
+    canvas.width = this.image.width
+    canvas.height = this.image.height
+
+    const ctx = canvas.getContext('2d')
+
+    ctx.drawImage(this.image, 0, 0)
+    ctx.clearRect(x1, y1, x2 - x1, y2 - y1)
+
+    ctx.canvas.toBlob(blob => {
+      const url = URL.createObjectURL(blob)
+
+      const image = new Image()
+      image.onload = () => {
+        this.image = image
+        this.display.ctx.clearRect(0, 0, this.screen_width, this.screen_height)
+        this.state_selection = null
+        this.drawImage(this.state_move_x, this.state_move_y, this.state_scale)
+      }
+
+      image.src = url
+    })
+  }
+
+  drawImage (x, y, scale) {
+    if (!this.image) return new Error('you must load image first')
+
+    const canvas_width = this.screen_width
+    const canvas_height = this.screen_height
+    const image_width = this.image.width
+    const image_height = this.image.height
+
+    const width = image_width * scale
+    const height = image_height * scale
+    const dx = (canvas_width - width) / 2 - x
+    const dy = (canvas_height - height) / 2 - y
+
+    this.background.ctx.clearRect(0, 0, canvas_width, canvas_height)
+    this.background.ctx.drawImage(this.image, dx, dy, width, height)
+  }
+
+  scale (scale) {
+    if (!this.image) return new Error('you must load image first')
+
+    if (!this.state_move_x) this.state_move_x = 0
+    if (!this.state_move_y) this.state_move_y = 0
+    if (!this.state_scale) this.state_scale = scale
+
+    this.drawImage(this.state_move_x, this.state_move_y, this.state_scale)
+  }
+
+  move (x, y) {
+    if (!this.image) return new Error('you must load image first')
+
+    if (!this.state_move_x) this.state_move_x = 0
+    if (!this.state_move_y) this.state_move_y = 0
+    if (!this.state_scale) this.state_scale = scale
+
+    this.state_move_x -= x
+    this.state_move_y -= y
+
+    this.drawImage(-this.state_move_x, -this.state_move_y, this.state_scale)
+  }
+}
+
+const editor = new Editor('#editor')
+
+editor.loadImage('./01GBHWVD9RY9W3Z2MN67TGMSBD.png')
